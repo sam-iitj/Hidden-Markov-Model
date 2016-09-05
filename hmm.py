6b80a07266c69d04004e36cd73fef9cdad1fdead
sam_iitj <pg201272009@iitj.ac.in>
2016-09-05 08:45:48 +0530

    Updates to README.md

--- hmm.py
+++ hmm.py
@@ -216,8 +216,8 @@ if __name__ == "__main__":
   pprint.pprint(A)
   pprint.pprint(B.T)
   print "\n\n"
-  print tabulate(zip([x[0] for x in sorted(hmm1.mapping.items(), key=lambda x:x[1])], B.T), headers=["Character", "Probability of being in each state"])
-  
+  print tabulate(zip([x[0] for x in sorted(hmm1.mapping.items(), key=lambda x:x[1])], B.T), headers=["Character", "Probability of being in each state"], tablefmt="fancy_grid")
+
   x = [ i + 1 for i in range(len(likilihood))]
   plt.plot(x, likilihood)
   plt.xlabel("Number of iterations")
